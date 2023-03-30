7bf6db5e727e3e03c376f617fabfbaf93b3972f5
Gunasekaran <78465620+Gunasekaran27@users.noreply.github.com>
2023-03-31 01:13:43 +0530

    added index.html

---
+++ app.js
@@ -0,0 +1,21 @@
+// var p = $('p');
+// var text = p.text().split(' ');
+// for (var i = 0, len = text.length; i < len; i = i + 1) {
+//     text[i] = '<mark>' + text[i] + '</mark>';
+// }
+// p.html(text.join(' '));
+
+// window.onscroll = function() {myFunction()};
+
+// var header = document.getElementById("myHeader");
+// var sticky = header.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
+
+
